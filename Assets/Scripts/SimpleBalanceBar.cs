e658e7084a454b73f10fd04d40eeee54afae93b5
Jacob <jacobfrance28@gmail.com>
2020-07-18 11:44:47 +1200

    Created balance bar script
    Balance bar is controlled via player balance script. simple Balance Bar script is applied to a game manager object and requires a UISlider and playerBalance Script

---
+++ Assets/Scripts/SimpleBalanceBar.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimpleBalanceBar : MonoBehaviour
+{
+   public Slider balanceBar;
+   public playerBalance playerBalance;
+
+void Update(){
+    balanceBar.value = playerBalance.currentBalance;
+}
+
+}
+
--- Assets/playerBalance.cs
+++ Assets/playerBalance.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class playerBalance : MonoBehaviour
 {
+    public float currentBalance = 0.0f;
+    public float maxRightLean = 45.0f;
+    public float maxleftLean = -45.0f;
+
+    public float counterBalanceStrength = 2.0f;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +18,25 @@ public class playerBalance : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        updateBalance();
     }
+
+
+void updateBalance(){
+    if(Input.GetKey(KeyCode.L)){
+            currentBalance += counterBalanceStrength;
+        }
+        if(Input.GetKey(KeyCode.K)){
+            currentBalance -= counterBalanceStrength;
+        }
+        
+        if(currentBalance <= maxleftLean){
+            Debug.Log("You fell to the left");
+        }
+        if(currentBalance >= maxRightLean){
+            Debug.Log("You fell off to the right");
+        }
+}
 }
