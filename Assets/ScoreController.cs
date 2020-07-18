3a8efccb8423b6448810bd2fc9c135a2b8b5c2e3
Jacob <jacobfrance28@gmail.com>
2020-07-18 17:51:09 +1200

    Implemented Basic Scoring System
    ScoreController is designed to be implemented via a game controller object. It requires a UI Text object for it to properly work as intended :)

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/////////////////////////////////////////////////////////////////////////////
+// This Script is intended to be implemented on a game controller object
+// but it could feasibly go anywhere really.
+// Make sure to include a UI Text object in your scene
+// Happy days :)
+////////////////////////////////////////////////////////////////////////////
+
+public class ScoreController : MonoBehaviour
+{
+
+    public float score = 100.00f;
+    public float scoreDecreasePerFrame = 0.1f;
+    public playerBalance pB;
+    public Text displayScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log(score.ToString().GetType());
+        displayScore.text = score.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(pB.currentBalance);
+        if(pB.currentBalance <= pB.maxLeftLean + 1 || pB.currentBalance >= pB.maxRightLean -1)
+        {
+            score -= scoreDecreasePerFrame;
+        }
+           //score-=1;
+        displayScore.text = score.ToString();
+            
+        
+    }
+}
