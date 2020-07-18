8f80c3e00357469d1ff8f6f5640c11481e9fb2a2
Jacob <jacobfrance28@gmail.com>
2020-07-18 14:38:26 +1200

    Wind object now affects player balance
    Add the windyboy script to any collider object and set that object to 'is trigger'

--- Assets/playerBalance.cs
+++ Assets/playerBalance.cs
@@ -6,9 +6,14 @@ public class playerBalance : MonoBehaviour
 {
     public float currentBalance = 0.0f;
     public float maxRightLean = 45.0f;
-    public float maxleftLean = -45.0f;
+    public float maxLeftLean = -45.0f;
 
     public float counterBalanceStrength = 2.0f;
+
+    //public float windStrength = 1.0f;
+    public bool isWindy = false;
+    public float windForce = 0f;
+
 
 
     // Start is called before the first frame update
@@ -20,23 +25,30 @@ public class playerBalance : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Debug.Log(rb.)
+        
         updateBalance();
     }
 
 
 void updateBalance(){
-    if(Input.GetKey(KeyCode.L)){
+    if(isWindy){
+        currentBalance += windForce;
+    }
+    
+    if(Input.GetKey(KeyCode.L) && currentBalance <= maxRightLean){
             currentBalance += counterBalanceStrength;
         }
-        if(Input.GetKey(KeyCode.K)){
-            currentBalance -= counterBalanceStrength;
-        }
-        
-        if(currentBalance <= maxleftLean){
-            Debug.Log("You fell to the left");
-        }
-        if(currentBalance >= maxRightLean){
-            Debug.Log("You fell off to the right");
-        }
+    if(Input.GetKey(KeyCode.K) && currentBalance >= maxLeftLean)
+    {
+        currentBalance -= counterBalanceStrength;
+    }
+    
+    if(currentBalance <= maxLeftLean){
+        Debug.Log("You fell to the left");
+    }
+    if(currentBalance >= maxRightLean){
+        Debug.Log("You fell off to the right");
+    }
 }
 }
---
+++ Assets/windyBoy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script is to be applied to objects with a collider attached.
+//DON'T FORGET TO SET THE COLLIDER TO 'IS TRIGGER'
+public class windyBoy : MonoBehaviour
+{
+    public playerBalance player;
+
+    //wind < 0 = towards Left
+    //wind > 0 = towards Right;
+    public float windDirection = 0f;
+    public float windStrength = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+        private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Helloworld");
+        player.isWindy = true;
+        player.windForce = this.windDirection * this.windStrength;
+    }
+    private void OnTriggerExit(Collider other) {
+        player.isWindy = false;
+        player.windForce = 0f;
+    }
+}
