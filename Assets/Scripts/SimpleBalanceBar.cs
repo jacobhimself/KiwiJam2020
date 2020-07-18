b9a803346a9c8db7780a7003f89f72b38920b7b6
Jacob <jacobfrance28@gmail.com>
2020-07-18 16:04:25 +1200

    simpleBalanceScript now auto sets min and max values of slider object

--- Assets/Scripts/SimpleBalanceBar.cs
+++ Assets/Scripts/SimpleBalanceBar.cs
@@ -8,6 +8,11 @@ public class SimpleBalanceBar : MonoBehaviour
    public Slider balanceBar;
    public playerBalance playerBalance;
 
+void Start() {
+    balanceBar.minValue = -45.0f;
+    balanceBar.maxValue = 45.0f;    
+}
+
 void Update(){
     balanceBar.value = playerBalance.currentBalance;
 }
