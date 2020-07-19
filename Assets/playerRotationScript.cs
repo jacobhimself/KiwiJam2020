36735cdb00b4d4e18192256668f6c8139368ad1f
Jacob <jacobfrance28@gmail.com>
2020-07-19 16:56:41 +1200

    Added player rotation based off balance meter

---
+++ Assets/playerRotationScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerRotationScript : MonoBehaviour
+{
+    public playerBalance pB;
+    public Transform playerMesh;
+    public Transform pivotal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        //playerMesh.transform.RotateAround(pivotal.position,new Vector3(0,0,1), pB.currentBalance);
+        playerMesh.transform.rotation = Quaternion.Euler(0,90,-pB.currentBalance);
+    }
+}
